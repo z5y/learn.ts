9f97aa77bd720338d26798797651a62a1315d33e
Hugo van Elk <hugo.van.elk@gmail.com>
2021-12-26 10:47:23 +0100

    s2.2 primitive types

---
+++ src/ts/s1_2_primitive_types.ts
@@ -0,0 +1,15 @@
+
+// Primitive types are in lower case.
+// All other types start with Uppercase letter.
+
+let isPresent: boolean = false;
+let magic: number = 66.6;
+let hello: string = 'world';
+
+let notDefined: any = undefined;
+let notDefined2: undefined= undefined;
+let notPresent: any = null;
+let notPresent2: null = null;
+
+let penta: symbol = Symbol('star');
+let biggy: bigint = 24n; // tsconfig.json target property
